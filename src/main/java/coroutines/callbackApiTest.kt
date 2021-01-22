6bd966432bcc39dd6bcce4ae480ffa52178f1a9d
Loner123 <dlwlstjq410@gmail.com>
2021-01-23 01:20:30 +0900

    [Add] Unit으로 변경

--- src/main/java/coroutines/callbackApiTest.kt
+++ src/main/java/coroutines/callbackApiTest.kt
@@ -5,17 +5,17 @@ import kotlinx.coroutines.*
 @ExperimentalCoroutinesApi
 fun main() {
     CoroutineScope(Dispatchers.IO).launch {
-        5.awaitCallback{ print("코루틴을 취소")}
+        "코루틴 정상 작동".awaitCallback{ print("코루틴을 취소")}
     }
     Thread.sleep(2000L)
 }
 
 @ExperimentalCoroutinesApi
-suspend fun <T> T.awaitCallback( onCancell: (T) -> Unit) = suspendCancellableCoroutine<T> {
+suspend fun <T> T.awaitCallback( onCancell: (T) -> Unit) = suspendCancellableCoroutine<Unit> {
 
    //캔슬
-    it.cancel()
-    it.resume(this) { onCancell(this) }
+//    it.cancel()
+    it.resume(Unit) { onCancell(this) }
 
     //캔슬 x 일때 실행
     if(!it.isCancelled)
